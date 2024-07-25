70a14dcb27912ff5940be12ab23dde1df5b24fff
KingNono1030 <hoejy0821@gmail.com>
2024-07-25 17:33:22 +0900

    feat: when formatDate takes invalid date, it returns string "invalid date"

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,5 +1,8 @@
 export default function formatDate(dateString: string): string {
   const date = new Date(dateString);
+  if (isNaN(date.getTime())) {
+    return 'invalid date';
+  }
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
